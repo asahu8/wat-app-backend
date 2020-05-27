f3dffab6b2581bd18ff246cfd019ee97aead60b2
ajays <asahu@castlighthealth.com>
2020-05-27 22:06:45 +0530

    Added event APIs

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -6,30 +6,32 @@ import { Event } from "../entity/Event";
 class EventController{
   static listAll = async(req: Request, res: Response) => {
     const eventRepository = getRepository(Event);
-    /*
-     TODO: Needs bit work to send required attributes to frontend
+
     const events = await eventRepository.find({
       select: [ "id", "name", "eventDate", "budget", "items", "description", "location", "active" ]
     });
     res.send(events);
-    */
-    res.status(201).send({
-      data: [
-        { id: 1, name: "Hearing Aids", date: "2020-02-10", createdBy: "ajays", commentsCount: 10, canView: true, canEdit: true, canDel: true },
-        { id: 2, name: "Books to Orphan age", date: "2020-01-10", createdBy: "mmuray", commentsCount: 20, canView: true, canEdit: true, canDel: true },
-        { id: 3, name: "Mountain Biking", date: "2020-05-10", createdBy: "alexh", commentsCount: 30, canView: true, canEdit: true, canDel: true },
-        { id: 4, name: "Summer camp", date: "2020-07-10", createdBy: "rberger", commentsCount: 12, canView: true, canEdit: true, canDel: true }
-      ]
-    });
+  };
 
+  static getOneById = async (req: Request, res: Response) => {
+    //Get the ID from the url
+    const id: string = req.params.id;
+
+    //Get the user from database
+    const eventRepository = getRepository(Event);
+    try {
+      const event = await eventRepository.findOneOrFail(id, {
+        select: [ "id", "name", "eventDate", "budget", "items", "description", "location", "active" ]
+      });
+      res.send(event);
+    } catch (error) {
+      res.status(404).send("Event not found");
+    }
   };
 
   static createEvent = async (req: Request, res: Response) => {
     const eventRepository = getRepository(Event);
     let eventData = req.body.event;
-    eventData.budget = 500;
-    eventData.items = "default item";
-    eventData.active = true;
     try {
       await eventRepository.save(eventData);
     } catch (e) {
@@ -37,8 +39,58 @@ class EventController{
       res.status(500).send("something went wrong");
       return;
     }
-    //If all ok, send 201 response
     res.status(201).send("Event created");
+  };
+
+  static deleteEvent = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const eventRepository = getRepository(Event);
+    let event: Event;
+    try {
+      event = await eventRepository.findOneOrFail(id);
+    } catch (error) {
+      res.status(404).send("Event not found");
+      return;
+    }
+    eventRepository.delete(id);
+    res.status(204).send();
+  }
+
+  static editEvent = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const { name, eventDate, budget, items, description, location, active } = req.body.event;
+    const eventRepository = getRepository(Event);
+    let event;
+
+    try {
+      event = await eventRepository.findOneOrFail(id);
+    } catch (error) {
+      res.status(404).send("Event not found");
+      return;
+    }
+
+    event.name = name;
+    event.eventDate = eventDate;
+    event.budget = budget;
+    event.items = items;
+    event.description = description;
+    event.location = location;
+    event.active = active;
+    const errors = await validate(event);
+    if (errors.length > 0) {
+      console.log(event);
+      res.status(400).send(errors);
+      return;
+    }
+
+    try {
+      await eventRepository.save(event);
+    } catch (e) {
+      console.log(e); // TODO: Remove this after adding synching all validations with frontend
+      res.status(409).send("some issue occured");
+      return;
+    }
+    res.status(204).send();
   };
 
   static listEventCards = async(req: Request, res: Response) => {
